69afdcb1822b62b18e063c4226e596eb3de2f1c3
arrajeevchandar <rajeevarchandar@gmail.com>
2025-08-23 07:56:11 +0530

    changed password

--- 2547101_Ex8/backend/db.js
+++ 2547101_Ex8/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "rajeev123",  // change this
+  password: "root",  // change this
   database: "erms_db"
 });
 
